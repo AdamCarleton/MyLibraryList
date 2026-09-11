035155012db1dfde7e939350fdb8ca8a91390586
Adam Carleton <acarleton15@gmail.com>
2026-09-10 19:19:26 -0500

    Add user login

--- routes/users/users.js
+++ routes/users/users.js
@@ -7,7 +7,7 @@ const User = require('../../models/user');
 
 router.get('/register', (req, res) => {
     res.render('users/register');
-})
+});
 
 router.post('/register', async (req, res) => {
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
@@ -20,6 +20,29 @@ router.post('/register', async (req, res) => {
 
     await user.save();
     res.redirect('/');
-})
+});
+
+router.get('/login', (req, res) => {
+    res.render('users/login');
+});
+
+router.post('/login', async (req, res) => {
+    const {username, password} = req.body;
+    const user = await User.findOne({ username: username });
+
+    if (!user) {
+        return res.send('Invalid username or password');
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+        res.send('Invalid username or password');
+    } 
+    else {
+        res.send(`Welcome back ${username}!`);
+    }
+    
+
+});
 
 module.exports = router;
