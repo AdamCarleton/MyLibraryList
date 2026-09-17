e7274856f1fd11669e4de4d6ecfef3700a384d69
Adam Carleton <acarleton15@gmail.com>
2026-09-16 19:36:34 -0500

    Add books to user's personal library

--- routes/books/books.js
+++ routes/books/books.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 
-// OPEN LIBRARY API DATA STRUCTURE
+// OPEN LIBRARY API DATA TO USE
 // docs [
 // {
     // - author_name: [a, b, c...]
@@ -12,6 +12,10 @@ const axios = require('axios');
     // - cover_i: 
 // }
 // ]
+
+// MODELS //
+const User = require('../../models/user');
+const Book = require('../../models/book');
 
 // API URLs //
 const searchURL = 'https://openlibrary.org/search.json?q=';
@@ -26,7 +30,7 @@ router.post('/search', async (req, res) => {
     const { titleSearch } = req.body;
 
     const response = await axios.get(`${searchURL}${titleSearch}`);
-    console.log(`${searchURL}${titleSearch}`);
+    // console.log(`${searchURL}${titleSearch}`);
     const books = response.data.docs;
         
     // const searchResult = books.map(book => {
@@ -48,8 +52,36 @@ router.post('/search', async (req, res) => {
             cover:cover_i
         });
     });
-    console.log(searchResult);
+    // console.log(searchResult);
     res.render('books/search-results', { searchResult, coverURL });
 });
+
+// ADD TO LIBRARY
+router.post('/add', async (req, res) => {
+    // Check if user is logged in, or redirect to login page
+    const currentUser = req.session.userId;
+    if (!currentUser) {
+        return res.redirect('/users/login');
+    }
+    const { title, author, year, cover } = req.body;
+    console.log(req.body);
+
+    // Add book data from form (add to library button) to a new document
+    const book = new Book({
+        title: title,
+        author: author,
+        year: year,
+        cover: cover,
+        owner: currentUser
+    });
+
+    await book.save();
+
+    // redirect to the user's library page
+    res.redirect('/users/library');
+
+});
+
+
 
 module.exports = router;
